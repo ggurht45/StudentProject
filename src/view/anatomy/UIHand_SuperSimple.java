e832ce9a7664505d312c3a15a7dfcf4bb3bb815c
Jahangir Iqbal <iqbal.jahangir45@gmail.com>
2017-07-05 19:52:43 +0100

    trying to use entireGroup as a container. ^^

--- src/view/anatomy/UIHand_SuperSimple.java
+++ src/view/anatomy/UIHand_SuperSimple.java
@@ -27,6 +27,7 @@ public class UIHand_SuperSimple extends UIHand {
     private Box hand;
     private Cylinder thumb;
     private Sphere fingers;
+    private Group entireGroup;
 
     public UIHand_SuperSimple(Color color, boolean wireframe) {
         super();
@@ -51,7 +52,9 @@ public class UIHand_SuperSimple extends UIHand {
 //        this.setRotate(20);
 
         //add children to UiHand group
-        this.getChildren().addAll(hand, thumb, fingers);
+        entireGroup = new Group();
+        entireGroup.getChildren().addAll(hand, thumb, fingers);
+        this.getChildren().add(entireGroup);
 
     }
 
@@ -63,31 +66,60 @@ public class UIHand_SuperSimple extends UIHand {
     //i must add a rotation transform at the end. that basically does a rotation around the y-axis. a 90 yaw. -> flipping -90 yaw.
     //why? because order is always pitch then yaw, then roll. so if you want to do yaw before pitch, it must come seperately.^^
 
-    private void tryAgain(Hand hand){
-        //determine correct p,y,r from direction:
-        Vector direction = new Vector(1,0,-1);
-        //new approach
+    private void tryAgain(Hand hand) {
+        System.out.println("***");
+        System.out.println("groupHand position before setGroup called in setLoc in superSimple");
+        System.out.println("layout: " + this.getLayoutX() + " " + this.getLayoutY());
+        System.out.println("translate: " + this.getTranslateX() + " " + this.getTranslateY() + " " + this.getTranslateZ());
+        System.out.println("this.getRotate(): " + this.getRotate());
+        System.out.println("this.getRotationAxis(): " + this.getRotationAxis());
+
+
+        //new approach, first hard set the the direction of the nodes in this hand.
         ViewMath.setGenericNode(this.hand, hand.stabilizedPalmPosition(), Vector.yAxis().times(20));
         ViewMath.setGenericNode(this.thumb, hand.stabilizedPalmPosition(), Vector.yAxis().times(20));
         ViewMath.setGenericNode(this.fingers, hand.stabilizedPalmPosition(), Vector.yAxis().times(20));
-        float roll = (float)Math.toRadians(0);
+
+
+        //determine correct p,y,r from direction:
+        Vector direction = new Vector(1, 0, -1);
+
+
+        float roll = (float) Math.toRadians(0);
         //flip sign for pitch and yaw before passing them as arguments.
         //remember pitch downwards is -90 or whatever angle. and yaw to the right is +30. or whatever angle.
         //now after you have thought of the angles as above. remember to flip them, as told below.
         //so if you imagined the pitch and yaw angles to be -90, 30, respectively, then you should pass them as 90, -30;
         //i think this may be b/c of jcs vs lcs. leap coordinate sys vs javafx cs.
-        float pitch = (float)Math.toRadians(45.0);
-        float yaw = (float)Math.toRadians(-45);
+        float pitch = (float) Math.toRadians(45.0);
+        float yaw = (float) Math.toRadians(-45);
         //stackoverflow: alf is roll, bet is pitch and gam is yaw.
-        ViewMath.matrixRotateNode(this, roll, pitch, yaw);
+        ViewMath.matrixRotateNode(this.entireGroup, roll, pitch, yaw);
+
         //fix translate
         fingers.getTransforms().add(new Translate(0, -6, 0)); // this transform happens first. transforms that get added last, are performed first
         thumb.getTransforms().add(new Translate(4, 0, 0));
 
         //add rotation around y-axis. yaw of 90 -> flip -> -90;
-        this.getTransforms().add(new Rotate(90, new Point3D(0,1,0)));
+        this.entireGroup.getTransforms().add(new Rotate(90, new Point3D(0, 1, 0)));
+
+        System.out.println("groupHand position After setGroup called in setLoc in superSimple");
+        System.out.println("layout: " + this.getLayoutX() + " " + this.getLayoutY());
+        System.out.println("translate: " + this.getTranslateX() + " " + this.getTranslateY() + " " + this.getTranslateZ());
+        System.out.println("this.getRotate(): " + this.getRotate());
+        System.out.println("this.getRotationAxis(): " + this.getRotationAxis());
+        System.out.println("***");
 
 
+        System.out.println("***2");
+        System.out.println("groupHand position After setGroup called in setLoc in superSimple");
+        System.out.println("layout: " + this.entireGroup.getLayoutX() + " " + this.entireGroup.getLayoutY());
+        System.out.println("translate: " + this.entireGroup.getTranslateX() + " " + this.entireGroup.getTranslateY() + " " + this.entireGroup.getTranslateZ());
+        System.out.println("this.getRotate(): " + this.entireGroup.getRotate());
+        System.out.println("this.getRotationAxis(): " + this.entireGroup.getRotationAxis());
+        System.out.println("***2");
+
+//        this.entireGroup.getTransforms().add(new Translate(-15, 0, 0));
 
     }
 
