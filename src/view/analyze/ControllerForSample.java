0b07e0ddb3d0721d7f7f6fec0a043f9f41282f97
Jahangir Iqbal <iqbal.jahangir45@gmail.com>
2017-07-16 14:59:15 +0100

    made fxml button clickable ^^

--- src/view/analyze/ControllerForSample.java
+++ src/view/analyze/ControllerForSample.java
@@ -1,5 +1,6 @@
 package view.analyze;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,6 +12,11 @@ public class ControllerForSample implements Initializable {
         System.out.println("Sample.fxml view is now loaded!");
     }
 
+
+    @FXML
+    private void sayaHelloa(){
+        System.out.println("yo yo wassup?!, the button has been clicked");
+    }
 
 
 }
