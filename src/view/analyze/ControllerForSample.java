37dd125c498f969b23f48307c7269b4ef885a2b6
Jahangir Iqbal <iqbal.jahangir45@gmail.com>
2017-07-18 20:23:42 +0100

    it works now! ^^

--- src/view/analyze/ControllerForSample.java
+++ src/view/analyze/ControllerForSample.java
@@ -1,13 +1,15 @@
 package view.analyze;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import model.SerializedTargetHand;
 import view.LeapUIApp;
 
@@ -46,7 +48,7 @@ public class ControllerForSample{
 
         //add a button to the vbox
         System.out.println("children of vbox" + theVBox.getChildren());
-        theVBox.getChildren().add(new JFXButton("yo"));
+        theVBox.getChildren().add(new JFXButton("yo"));//note interesting effect when you do setAll vs add
     }
 
     @FXML
@@ -62,6 +64,24 @@ public class ControllerForSample{
     private VBox theVBox;
 
     @FXML
+    private TreeTableView<String> treeTableView;
+
+    @FXML
+    private TreeTableColumn<String, String> ttCol;
+
+
+
+    //fake data for tt in tab2
+    TreeItem<String> item0 = new TreeItem<>("Daniel");
+    TreeItem<String> item1 = new TreeItem<>("Joe");
+    TreeItem<String> item2 = new TreeItem<>("Alice");
+    TreeItem<String> item3 = new TreeItem<>("Bob");
+    TreeItem<String> item4 = new TreeItem<>("Braxton");
+
+    TreeItem<String> root = new TreeItem<>("root");
+
+
+    @FXML
     public void initialize() throws Exception {
 
         //add a button to the vbox, after everything has been done in fxml.
@@ -70,9 +90,38 @@ public class ControllerForSample{
 
         //lets add some thing we prepared in another fxml file
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("pomo.fxml"));
-        Parent root = fxmlLoader.load();
-        theVBox.getChildren().add(root);
+        Parent button2 = fxmlLoader.load();
+        theVBox.getChildren().add(button2);
 
+
+
+        //add some data to the treetableView shown in tab2
+        root.getChildren().setAll(item0, item1, item2, item3, item4);
+
+
+        ttCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<String, String>, ObservableValue<String>>() {
+            @Override
+            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<String, String> param) {
+                return new SimpleStringProperty(param.getValue().getValue());
+            }
+
+        });
+
+        //do some weird stuff to col, but necessary
+//        ttCol.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<String, String>, ObservableValue<String>>() {
+//            @Override
+//            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<String, String> param) {
+//                return new SimpleStringProperty(param.getValue().getValue());
+//            }
+//        });
+
+
+        //doing weird stuff with lambdas
+//        ttCol.setCellValueFactory((TreeTableColumn.CellDataFeatures<String, String> param) -> new SimpleStringProperty(param.getValue().getValue()));
+
+
+
+        treeTableView.setRoot(root);
     }
 
 
